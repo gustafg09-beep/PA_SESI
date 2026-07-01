65c3c9983d4f7948a04ee61b235c890aee4c444d
gustavo <gustafg09@gmail.com>
2026-07-01 07:47:39 -0300

    ok

--- Trabalho - Programação de aplicativos/connection.php
+++ Trabalho - Programação de aplicativos/connection.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $user = "root";
 $password = "root";
-$database = "db_usuario";
+$database = "escola";
 $port = 3307;
 
 $conn = mysqli_connect($server, $user, $password, $database, $port);
